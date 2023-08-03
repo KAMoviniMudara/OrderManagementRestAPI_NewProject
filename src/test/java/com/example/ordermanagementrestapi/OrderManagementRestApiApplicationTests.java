eb070b0db847b2327e4b21232ff7b7ca5dbf9810
KAMoviniMudara <mudaramovini@gmail.com>
2023-08-03 07:43:08 +0530

    Complete Entity package

--- src/test/java/com/example/ordermanagementrestapi/OrderManagementRestApiApplicationTests.java
+++ src/test/java/com/example/ordermanagementrestapi/OrderManagementRestApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class OrderManagementRestApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
