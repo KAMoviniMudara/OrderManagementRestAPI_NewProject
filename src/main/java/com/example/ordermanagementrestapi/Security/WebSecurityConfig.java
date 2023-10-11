df99332f2a46e5d3598a9ae639f27ac8d55bc0ec
KAMoviniMudara <mudaramovini@gmail.com>
2023-10-11 07:11:28 +0530

    Complete Login

--- src/main/java/com/example/ordermanagementrestapi/Security/WebSecurityConfig.java
+++ src/main/java/com/example/ordermanagementrestapi/Security/WebSecurityConfig.java
@@ -60,8 +60,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
-                .antMatchers("/api/v1/auth/**").permitAll()
-                .antMatchers("/api/v1/test/**").permitAll()
+                .antMatchers("/api/auth/**").permitAll()
+                .antMatchers("/api/test/**").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
